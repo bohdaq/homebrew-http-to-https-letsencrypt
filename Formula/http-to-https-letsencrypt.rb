99a8b005c4cc9340a3b37dfd53060c8832b5b035
Bohdan Tsap <bohdan.tsap@tutanota.com>
2022-12-28 23:13:49 +0200

    updated homepage

--- Formula/http-to-https-letsencrypt.rb
+++ Formula/http-to-https-letsencrypt.rb
@@ -1,6 +1,6 @@
 class HttpToHttpsLetsencrypt < Formula
   desc "http-to-https-letsencrypt is an application web-server for making permanent redirects from http to https."
-  homepage "https://github.com/bohdaq/rust-http-to-https-letsencrypt-acme"
+  homepage "https://github.com/bohdaq/http-to-https-letsencrypt"
   url "https://github.com/bohdaq/http-to-https-letsencrypt/archive/refs/tags/8.0.8.tar.gz"
   sha256 "790e2fed54c10d1950a91becb603af3ef6e31f4820732b9717276c2e4507bb25"
   head "https://github.com/bohdaq/http-to-https-letsencrypt.git", branch: "main"
