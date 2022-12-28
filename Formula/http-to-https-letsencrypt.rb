c3016a68af729c4f5a133f23ba24157d697a52ae
Bohdan Tsap <bohdan.tsap@tutanota.com>
2022-12-28 21:42:11 +0200

    rolling release

--- Formula/http-to-https-letsencrypt.rb
+++ Formula/http-to-https-letsencrypt.rb
@@ -3,6 +3,7 @@ class HttpToHttpsLetsencrypt < Formula
   homepage "https://github.com/bohdaq/rust-http-to-https-letsencrypt-acme"
   url "https://github.com/bohdaq/rust-http-to-https-letsencrypt-acme/archive/refs/tags/8.0.0.tar.gz"
   sha256 "f6685f5311fb6aca7ad8c080663881d0609b2f88da39d3bf32d0bbb34263416f"
+  head "https://github.com/bohdaq/http-to-https-letsencrypt.git", branch: "main"
   license "MIT"
 
   depends_on "rust" => :build
--- README.md
+++ README.md
@@ -1,7 +1,15 @@
 Homebrew Formula for http-to-https-letsencrypt
 
+> brew uninstall http-to-https-letsencrypt
+>
+> brew untap bohdaq/http-to-https-letsencrypt
+>
+> brew update
+>
 > brew tap bohdaq/http-to-https-letsencrypt
 >
-> brew install http-to-https-letsencrypt
+> brew install bohdaq/http-to-https-letsencrypt/http-to-https-letsencrypt --HEAD
 >
 > http-to-https-letsencrypt
+
+Press Ctrl + C (or CMD + C) to stop server.
