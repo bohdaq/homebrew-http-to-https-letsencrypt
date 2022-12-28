0d7d7414bada83ac962a63fbe273425bb3b5c948
Bohdan Tsap <bohdan.tsap@tutanota.com>
2022-12-28 21:45:39 +0200

    updated url and sha

--- Formula/http-to-https-letsencrypt.rb
+++ Formula/http-to-https-letsencrypt.rb
@@ -1,8 +1,8 @@
 class HttpToHttpsLetsencrypt < Formula
   desc "http-to-https-letsencrypt is an application web-server for making permanent redirects from http to https."
   homepage "https://github.com/bohdaq/rust-http-to-https-letsencrypt-acme"
-  url "https://github.com/bohdaq/rust-http-to-https-letsencrypt-acme/archive/refs/tags/8.0.0.tar.gz"
-  sha256 "f6685f5311fb6aca7ad8c080663881d0609b2f88da39d3bf32d0bbb34263416f"
+  url "https://github.com/bohdaq/http-to-https-letsencrypt/archive/refs/tags/8.0.8.tar.gz"
+  sha256 "790e2fed54c10d1950a91becb603af3ef6e31f4820732b9717276c2e4507bb25"
   head "https://github.com/bohdaq/http-to-https-letsencrypt.git", branch: "main"
   license "MIT"
 
